ddeabe4c0d09e24add3b55ae7e51762262cfb328
ROBERTO-CARLOS-PACO-LIMACHI <118031754+ROBERTO-CARLOS-PACO-LIMACHI@users.noreply.github.com>
2025-10-02 06:52:10 -0400

    Actualizar conn.js

--- config/conn.js
+++ config/conn.js
@@ -13,7 +13,7 @@ export  async function obtenerToken() {
         }),
       },
     );
-   console.log("datos del respuesta:", respuesta)
+   console.log("datos del respuesta usuariocontraseña:", proccess.env.username," ",process.env.password)
     if (!respuesta.ok) {
       
       throw new Error("Error en la solicitud");
