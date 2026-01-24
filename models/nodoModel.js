2108d45fc09f1e723a979405525364c67aed466a
ROBERTO-CARLOS-PACO-LIMACHI <robertocarlospacolimachi@gmail.com>
2026-01-23 21:52:24 -0400

    prueba 12 23-01-2026

--- models/nodoModel.js
+++ models/nodoModel.js
@@ -303,7 +303,9 @@ export class NodeModel {
     console.log('conectado al endpoint de agregar datos')
     console.log(datos)
     //const result=await guardarDB(datos)
-    console.log("data x: ",datos.eje_x)
+    console.log("data x: ",typeof datos.eje_x)
+    console.log("data y: ",typeof datos.eje_y)
+    console.log("data z: ",typeof datos.eje_z)
     const mapEje=eje=>
       eje.map((value,i)=>({value,timestamp:datos.timestamp[i]}))
     const resultado={
