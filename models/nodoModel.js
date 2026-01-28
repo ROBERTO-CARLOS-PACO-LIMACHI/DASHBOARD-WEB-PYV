551ed2eb3a12f00e99ab76627a86b61c8af32f59
ROBERTO-CARLOS-PACO-LIMACHI <robertocarlospacolimachi@gmail.com>
2026-01-28 19:58:09 -0400

    prueba 14 28012026

--- models/nodoModel.js
+++ models/nodoModel.js
@@ -322,11 +322,11 @@ export class NodeModel {
 
     const mapEje=(eje=[],timestamp=[])=>{
       console.log("eje a convertir: ",eje)
-      return eje.map((value,i)=>({value:2*(9.81)*Math.abs(value)/32768,timestamp:timestamp[i]}))
+      return eje.map((value,i)=>({value:2*(9.81)*Math.abs(value)/32768,timestamp:new Date(timestamp[i])}))
     }
     const mapEjez=(eje=[],timestamp=[])=>{
       console.log("eje a convertir: ",eje)
-      return eje.map((value,i)=>({value:Math.abs(2*(9.81)*Math.abs(value)/32768-9.81),timestamp:timestamp[i]}))
+      return eje.map((value,i)=>({value:Math.abs(2*(9.81)*Math.abs(value)/32768-9.81),timestamp:new Date(timestamp[i])}))
     }
     const resultado={
       eje_x:mapEje(datos.eje_x,timestamp),
@@ -343,7 +343,7 @@ export class NodeModel {
     //console.log("prueba 1 pasada ",resultado)
     const vector_suma=Object.keys(datos.eje_x).map(k=>({
       value:Math.abs(2*(9.81)*(datos.eje_x[k]+datos.eje_y[k]+datos.eje_z[k])/32768-9.81),
-      timestamp:timestamp[k]
+      timestamp:new Date(timestamp[k])
    }))
     resultado.vector_suma=vector_suma
     console.log("datos convetidos: ",resultado)
