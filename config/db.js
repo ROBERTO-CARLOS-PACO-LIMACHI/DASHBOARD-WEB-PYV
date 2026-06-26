4119f839bb1834b8bf0002fe9f7eee800f228b24
ROBERTO-CARLOS-PACO-LIMACHI <118031754+ROBERTO-CARLOS-PACO-LIMACHI@users.noreply.github.com>
2026-06-25 23:10:17 -0400

    Actualizar db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const conectarDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://roberto:abc123$@ac-iva4srm-shard-00-00.uypodwv.mongodb.net:27017,ac-iva4srm-shard-00-01.uypodwv.mongodb.net:27017,ac-iva4srm-shard-00-02.uypodwv.mongodb.net:27017/?ssl=true&replicaSet=atlas-740bbj-shard-0&authSource=admin&appName=Cluster0",
+      "mongodb://roberto:abc123$@ac-iva4srm-shard-00-00.uypodwv.mongodb.net:27017,ac-iva4srm-shard-00-01.uypodwv.mongodb.net:27017,ac-iva4srm-shard-00-02.uypodwv.mongodb.net:27017/db-sismico?ssl=true&replicaSet=atlas-740bbj-shard-0&authSource=admin&appName=Cluster0",
     );
     console.log("conectado a la base de datos");
   } catch (err) {
